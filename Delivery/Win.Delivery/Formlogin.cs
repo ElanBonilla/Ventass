8550a5d252c87a7533b9bdbbb56ca45b5fe8977e
Carolina <Carolina@DESKTOP-8J37C11>
2021-06-23 11:34:50 -0600

    Tarea 3 24/06/21

--- Delivery/Win.Delivery/Formlogin.cs
+++ Delivery/Win.Delivery/Formlogin.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BL.Ventas;
+using System;
 using System.Windows.Forms;
 
 namespace Win.Delivery
 {
     public partial class FormLogin : Form
     {
+        SeguridadBL _seguridad;
+
         public FormLogin()
         {
             InitializeComponent();
+            _seguridad = new SeguridadBL();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -24,13 +21,16 @@ namespace Win.Delivery
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             string usuario;
             string contrasena;
 
             usuario = textBox1.Text;
             contrasena = textBox2.Text;
 
-            if (usuario == "admin" && contrasena == "123")
+            var resultado = _seguridad.Autorizar(usuario, contrasena);
+
+            if (resultado == true)
             {
                 this.Close();
             }
